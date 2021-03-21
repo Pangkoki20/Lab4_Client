2b20ee746b590270e3dbfd88bae8ff310261c5ec
Pangkoki20 <sasithorn.pangkoki@gmail.com>
2021-03-21 19:57:39 +0700

    Session

--- pages/index.js
+++ pages/index.js
@@ -63,30 +63,30 @@ import styles from '../styles/Home.module.css'
 
 // ---------------------------------------------------------------------------------------------------------- //
 //Session
-const express = require('express')
-const app = express()
-const session = require('express-session')
-// sign cookie (for a session)
-app.use(session({
-  secret: 'keyboard cat', cookie: { maxAge: 60000 },
-  resave: false, saveUninitialized: false
-}))
-// resave => Forces the session to be saved back to the session store, even if the session was never modified
-// saveUninitialized => the cookie will not be set on a response with an uninitialized session
-app.use(function (req, res, next) {
-  var sess = req.session
-  if (sess.views) {
-    sess.views++
-  } else {
-    sess.views = 1
-  }
-  console.log(sess.views)
-  next();
-})
-app.get('/', function (req, res) {
-  res.send('count =' + req.session.views)
-})
-app.listen(3000)
+// const express = require('express')
+// const app = express()
+// const session = require('express-session')
+// // sign cookie (for a session)
+// app.use(session({
+//   secret: 'keyboard cat', cookie: { maxAge: 60000 },
+//   resave: false, saveUninitialized: false
+// }))
+// // resave => Forces the session to be saved back to the session store, even if the session was never modified
+// // saveUninitialized => the cookie will not be set on a response with an uninitialized session
+// app.use(function (req, res, next) {
+//   var sess = req.session
+//   if (sess.views) {
+//     sess.views++
+//   } else {
+//     sess.views = 1
+//   }
+//   console.log(sess.views)
+//   next();
+// })
+// app.get('/', function (req, res) {
+//   res.send('count =' + req.session.views)
+// })
+// app.listen(3000)
 
 
 
