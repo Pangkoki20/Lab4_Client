a4cc8ca414f4e2b6d466c806a40a03006c7343fd
Pangkoki20 <sasithorn.pangkoki@gmail.com>
2021-03-21 20:08:12 +0700

    Create Api

--- pages/index.js
+++ pages/index.js
@@ -88,7 +88,25 @@ import styles from '../styles/Home.module.css'
 // })
 // app.listen(3000)
 
+// ---------------------------------------------------------------------------------------------------------- //
+//การสร้าง API ในการส่ง Task
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const PORT = 8000
+let tasks = [
+  { id: 1, name: 'Do homework' },
+  { id: 2, name: 'Read book' },
+  { id: 3, name: 'Write a program' }]
+app.use(cors())
+app.get('/', (req, res) => {
+  res.json(tasks)
+})
+app.listen(PORT, () => console.log(`listen at ${PORT}`))
 
+
+
+// ---------------------------------------------------------------------------------------------------------- //
 
 export default function Home() {
   return (
