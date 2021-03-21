c2ef836f8900eb149d41724f0a4d3878f207b926
Pangkoki20 <sasithorn.pangkoki@gmail.com>
2021-03-21 19:39:59 +0700

    Session

--- pages/index.js
+++ pages/index.js
@@ -48,19 +48,46 @@ import styles from '../styles/Home.module.css'
 
 // ---------------------------------------------------------------------------------------------------------- //
 //Cookie ใช้เก็บข้อมูลฝั่งผู้ใช้ ผ่าน middleware
+// const express = require('express')
+// const app = express()
+// const cookieParser = require('cookie-parser')
+// app.use(cookieParser('keyboard cat')) //‘keyboard cat’ is a secret key to sign cookie (prevent cookie tamper)
+// app.get('/ck_get', function (req, res) {
+//   res.send(req.cookies)
+// })
+// app.get('/ck_set', function (req, res) {
+//   res.cookie('a', 10)
+//   res.send('ok')
+// })
+// app.listen(3000)
+
+// ---------------------------------------------------------------------------------------------------------- //
+//Session
 const express = require('express')
 const app = express()
-const cookieParser = require('cookie-parser')
-app.use(cookieParser('keyboard cat')) //‘keyboard cat’ is a secret key to sign cookie (prevent cookie tamper)
-app.get('/ck_get', function (req, res) {
-  res.send(req.cookies)
+const session = require('express-session')
+// sign cookie (for a session)
+app.use(session({
+  secret: 'keyboard cat', cookie: { maxAge: 60000 },
+  resave: false, saveUninitialized: false
+}))
+// resave => Forces the session to be saved back to the session store, even if the session was never modified
+// saveUninitialized => the cookie will not be set on a response with an uninitialized session
+app.use(function (req, res, next) {
+  var sess = req.session
+  if (sess.views) {
+    sess.views++
+  } else {
+    sess.views = 1
+  }
+  console.log(sess.views)
+  next();
 })
-app.get('/ck_set', function (req, res) {
-  res.cookie('a', 10)
-  res.send('ok')
+app.get('/', function (req, res) {
+  res.send('count =' + req.session.views)
 })
 app.listen(3000)
-// ---------------------------------------------------------------------------------------------------------- //
+
 
 
 export default function Home() {
